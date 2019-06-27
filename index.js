a0114cf16d8567b53176e5b26565286432b6ba6b
Christoffer Palm <christoffer.palm@oddhill.se>
2019-06-27 14:31:32 +0200

    Removed usage of http

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const app = express()
 const session = require('express-session')
-const MongoStore = require('connect-mongo')(session);
-const settings = require('./config/settings.json');
-const http = require('http')
+const MongoStore = require('connect-mongo')(session)
+const settings = require('./config/settings.json')
 const connectToDb = require('./config/db')
 const config = require('./config/config')
 const userRoutes = require('./API/userRoutes')
@@ -14,42 +12,34 @@ const productRoutes = require('./API/productRoutes')
 const eventRoutes = require('./API/eventRoutes')
 const mailRoutes = require('./API/mailRoutes')
 const loginRoutes = require('./API/loginRoutes')
-const path = require('path')
 
-const TojjServer = () => {
-  connectToDb()
-  app.use(bodyParser.json());
-  app.get('/', (req, res) => res.send('Welcome To Tojj Server'))
-  global.salt = settings.salt
+connectToDb()
 
-  app.use(session({
-    secret: settings.cookieSecret,
-    resave: true,
-    saveUninitialized: true,
-    store: new MongoStore({
-      mongooseConnection: global.db
-    })
-  }))
+const app = express()
 
-  app.use(userRoutes,
-    qnaRoutes,
-    fundraiserRoutes,
-    productRoutes,
-    eventRoutes,
-    mailRoutes,
-    loginRoutes
-  )
-  
-  app.use(express.static(path.join(__dirname, 'build')));
+app.use(bodyParser.json());
+app.get('/', (req, res) => res.send('Welcome To Tojj Server'))
+global.salt = settings.salt
 
- /* app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  }); */
+app.use(session({
+  secret: settings.cookieSecret,
+  resave: true,
+  saveUninitialized: true,
+  store: new MongoStore({
+    mongooseConnection: global.db
+  })
+}))
 
-  const server = http.Server(app)
-  server.listen(config.PORT, () => console.log(`Tojj Server is on port ${config.PORT}`))
-}
+app.use(userRoutes,
+  qnaRoutes,
+  fundraiserRoutes,
+  productRoutes,
+  eventRoutes,
+  mailRoutes,
+  loginRoutes
+)
 
+// app.use(express.static(path.join(__dirname, 'build')));
 
-TojjServer()
+app.listen(config.PORT, () => console.log(`Tojj Server is on port ${config.PORT}`))
 
